173e15f841a4ca0c622d8474d43cc31193e6d801
yousaforever <3624570292@qq.com>
2024-02-19 15:58:41 +0800

    封装函数

--- ddns.py
+++ ddns.py
@@ -3,27 +3,33 @@ import http.client
 import json
 from datetime import datetime
 
-# 清除ip.txt的内容
-open('ip.txt', 'w').close()
+def clear_ip_file():
+    # 清除ip.txt的内容
+    open('ip.txt', 'w').close()
 
-# 发送GET请求获取IP地址
-url = 'http://api.ipify.org'
-response = requests.get(url)
+def get_ip_and_save():
+    url = 'http://api.ipify.org'
+    response = requests.get(url)
 
-if response.status_code == 200:
-    # 将IP地址保存到ip.txt文件中
-    with open('ip.txt', 'w') as file:
-        file.write(response.text + '\n')
-    
-    # 将当前时间和IP地址保存到logs.txt文件中
-    now = datetime.now()
-    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-    with open('logs.txt', 'a') as log_file:
-        log_file.write(f"{current_time} - {response.text}\n")
-    
-    print('IP地址已保存到ip.txt文件中，并已保存日志到logs.txt文件中。')
+    if response.status_code == 200:
+        with open('ip.txt', 'w') as file:
+            file.write(response.text + '\n')
 
-    # 上传IP地址到Cloudflare
+        now = datetime.now()
+        current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+        with open('logs.txt', 'a') as log_file:
+            log_file.write(f"{current_time} - {response.text}\n")
+
+        print('IP地址已保存到ip.txt文件中，并已保存日志到logs.txt文件中。')
+    else:
+        print('获取IP地址失败。')
+
+def get_current_ip():
+    with open('ip.txt', 'r') as file:
+        ip = file.readline().strip()
+    return ip
+
+def dns_update_ip(ip):
     CF_Email = "alice@example.com"
     CF_Token = "0932a09c9a9d"
     CF_Zone_ID = "a900b9a9d8a9"
@@ -41,7 +47,7 @@ if response.status_code == 200:
     payload = {
         "type": "A",
         "name": "example.com",  # 修改为你的域名
-        "content": response.text,
+        "content": ip,
         "ttl": 120,
     }
 
@@ -51,5 +57,12 @@ if response.status_code == 200:
     data = res.read()
 
     print(data.decode("utf-8"))
-else:
-    print('获取IP地址失败。')
+
+def main():
+    clear_ip_file()
+    get_ip_and_save()
+    ip = get_current_ip()
+    dns_update_ip(ip)
+
+if __name__ == '__main__':
+    main()
